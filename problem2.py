1f2935f1161ce714c1385fc4bfb5da9ec3c8041b
Gayathri-129 <131529701+Gayathri-129@users.noreply.github.com>
2023-06-29 22:14:26 +0530

    Add files via upload
    problem 2 completed

---
+++ problem2.py
@@ -0,0 +1,29 @@
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list) // 2
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
+if __name__ == "__main__":
+    list = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+    sorted_list = merge_sort(list)
+    print(sorted_list)
